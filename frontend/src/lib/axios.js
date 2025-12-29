0215224b40ec53c5a0563bee5e858b9a2d7caa4a
Aman Parida <amantechz467@gmail.com>
2025-12-29 20:44:34 +0530

    Added backend render link

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://chatzone-oou0.onrender.com/api",
     withCredentials: true
 });
 
