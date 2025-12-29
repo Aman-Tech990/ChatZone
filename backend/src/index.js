316c478886fa35c31fe8764b381c19e90f03bc1a
Aman Parida <amantechz467@gmail.com>
2025-12-29 20:41:48 +0530

    Added vercel frontend link

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 5005;
 
 // CORS Setup
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://chatzone-henna.vercel.app"
+    ],
     credentials: true
 }));
 
