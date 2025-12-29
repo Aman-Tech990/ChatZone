698647b8e1f976192b67103aae3eb6d65f5bd208
Aman Parida <amantechz467@gmail.com>
2025-12-29 20:56:11 +0530

    Cookie setup for vercel+render

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,6 +9,8 @@ export const generateToken = (userId, res) => {
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "none",   
+        secure: true,      
     });
 
     return token;
